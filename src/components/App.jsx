a323c802b11d0549e9edff61783b7260ebdb0509
Jake Kemple <jakek927@gmail.com>
2018-02-03 20:09:12 -0800

    Moved navbar into App component class, began styling, added active class styling and functionality on navbar tabs, created test object array with faker

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,19 +1,44 @@
 import React from 'react';
-import { BrowserRouter, Route, Link, Switch } from 'react-router-dom'
+import { BrowserRouter, Route, Link, Switch } from 'react-router-dom';
+
 import InfoTable from './InfoTable.jsx';
 import AwesomeComponent from './AwesomeComponent.jsx';
 
 class App extends React.Component {
-  render() {
-    return (
-        <BrowserRouter>
-          <Switch>
-            <Route exact path="/" component={InfoTable} />
-            <Route exact path="/Awesome" component={AwesomeComponent} />
-          </Switch>
-        </BrowserRouter>
-    );
-  }
+    constructor(props) {
+        super(props);
+        this.setActiveTab= this.setActiveTab.bind(this);
+    }
+
+    setActiveTab(tab) {
+        var activeTab = document.getElementsByClassName("active");
+        while (activeTab.length) {
+            activeTab[0].classList.remove("active");
+        }
+        var currentElement = document.getElementById(tab.target.closest('a').id);
+        currentElement.classList.add("active");
+    }
+
+    render() {
+        return (
+            <nav id="navbar">
+                <BrowserRouter>
+                    <div>
+                    <ul>
+                        <li className="nav-tabs"><Link to="/" id="info-table-tab" onClick={((e) => this.setActiveTab(e))}>Info Table</Link></li>
+                        <li className="nav-tabs"><Link to="/Awesome" id="second-tab" onClick={((e) => this.setActiveTab(e))}>Test Link</Link></li>
+                        <li className="nav-tabs"><Link to="#" id="third-tab" onClick={((e) => this.setActiveTab(e))}>Dead Link</Link></li>
+                    </ul>
+            
+                    <Switch>
+                        <Route exact path="/" component={InfoTable} />
+                        <Route exact path="/Awesome" component={AwesomeComponent} />
+                    </Switch>
+                    </div>
+                </BrowserRouter>
+            </nav>  
+        );
+    }
 }
 
 export default App;
--- src/components/InfoTable.jsx
+++ src/components/InfoTable.jsx
@@ -1,33 +1,72 @@
 import React from 'react';
 
 class InfoTable extends React.Component {
+    constructor(props) {
+        super(props);
+        this.generateObjectArray= this.generateObjectArray.bind(this);
+      }
+
+    generateObjectArray() {
+        var faker = require('faker');
+        var a = [];
+        
+        for(var i=0; i<100; i++) {
+            var newUser = { 
+                firstname : faker.name.firstName(),
+                lastname : faker.name.lastName(),
+                country : faker.address.country(),
+                address : faker.address.streetName(),
+                city : faker.address.city(),
+                state : faker.address.state(),
+                zip : faker.address.zipCode(),
+                phone : faker.phone.phoneNumber()
+            };
+            a.push(newUser);
+        }
+        console.log(a);
+    }
+
+
     render () {
         return (
             // Hardcoded table data
             <div>
-                <h1>List of Awesome | Sort by: [] </h1>
-                <table>
-                <tbody>
-                    <tr>
-                        <th>Firstname</th>
-                        <th>Lastname</th> 
-                        <th>Country</th>
-                        <th>Address</th>
-                        <th>City</th>
-                        <th>State</th>
-                        <th>Zip</th>
-                        <th>Phone</th>                       
-                    </tr>
-                    <tr>
-                        <th>Firstname2</th>
-                        <th>Lastname2</th> 
-                        <th>Country2</th>
-                        <th>Address2</th>
-                        <th>City2</th>
-                        <th>State2</th>
-                        <th>Zip2</th>
-                        <th>Phone2</th>                       
-                    </tr>
+                <button onClick={this.generateObjectArray}>Click Me</button>
+                <table id="info-table" cellSpacing="0">
+                    <caption id="info-table-caption"><span>List of Awesome</span><span>|</span><span id="sort-type">Sort by: [] </span></caption>
+                    <thead className="table-header">
+                        <tr>
+                            <th>Firstname</th>
+                            <th>Lastname</th> 
+                            <th>Country</th>
+                            <th>Address</th>
+                            <th>City</th>
+                            <th>State</th>
+                            <th>Zip</th>
+                            <th>Phone</th>                       
+                        </tr>
+                    </thead>
+                    <tbody>
+                        <tr className="table-row">
+                            <td>Firstname</td>
+                            <td>Lastname</td> 
+                            <td>Country</td>
+                            <td>Address</td>
+                            <td>City</td>
+                            <td>State</td>
+                            <td>Zip</td>
+                            <td>Phone</td>                       
+                        </tr>
+                        <tr className="table-row">
+                            <td>Firstname</td>
+                            <td>Lastname</td> 
+                            <td>Country</td>
+                            <td>Address</td>
+                            <td>City</td>
+                            <td>State</td>
+                            <td>Zip</td>
+                            <td>Phone</td>                        
+                        </tr>
                     </tbody>
                 </table>                
             </div>
